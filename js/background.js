1cc40420be044703f7a2f6cf958cfa7bdace1ba1
David Troyer <dmtroyer@gmail.com>
2013-05-05 13:58:58 -0400

    Initial Push

---
+++ js/background.js
@@ -0,0 +1,34 @@
+var GOOD_RATIO = 1;
+
+function backgroundSocket() {
+  var dispatcher = new WebSocketRails('ws://10.118.182.171:3000/websocket');
+  dispatcher.on_open = function(data) {
+    // console.log('Connection has been established: ' + data);
+    console.log('background socket opened');
+  }
+
+  function addBioMetricDataToDom(data) {
+    // console.log('just received new biometric data: ' + data);
+    console.log("received data: " + data);
+    var object = JSON.parse(data);
+    lastMessage = object;
+    console.log(object.standing);
+    var ratio = (object.standing + object.walking) / object.sitting;
+    console.log("ratio: " + ratio)
+    
+    if (ratio < GOOD_RATIO)
+    {
+      chrome.browserAction.setIcon({path: "female_red_icon.png"});
+    }
+    else
+    {
+      chrome.browserAction.setIcon({path: "female_green_icon.png"});
+    }
+  }
+  var channel = dispatcher.subscribe('biometrics')
+  channel.bind('updated_biometrics', addBioMetricDataToDom);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  backgroundSocket();
+});
---
+++ js/popup.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+  openWebSocket();
+});
+
+function openWebSocket() {
+  var dispatcher = new WebSocketRails('ws://10.118.182.171:3000/websocket');
+  dispatcher.on_open = function(data) {
+    console.log('socket opened');
+  }
+
+  function addBioMetricDataToDom(data) {
+    console.log("received data: " + data);
+    var parsedData = JSON.parse(data);
+    var totalSeconds = parsedData.standing + parsedData.walking + parsedData.sitting;
+    var goodBarPercentage = Math.round((parsedData.standing + parsedData.walking) / totalSeconds * 100);
+    var badBarPercentage = Math.round(parsedData.sitting / totalSeconds * 100);
+    var ratio = (parsedData.standing + parsedData.walking) / parsedData.sitting;
+
+    var imgPath = ratio < 1 ? "sad_lady.png" : "happy_lady.png";
+    console.log("ratio: " + ratio)
+    console.log(imgPath);
+
+    document.getElementById('today_img').src = imgPath;
+    document.getElementById('good_bar').setAttribute('style', "width: " + goodBarPercentage + "%;");
+    document.getElementById('bad_bar').setAttribute('style', "width: " + badBarPercentage + "%;");
+  }
+  var channel = dispatcher.subscribe('biometrics')
+  channel.bind('updated_biometrics', addBioMetricDataToDom);
+}
+
+// function clickHandler(e) { document.getElementById("hello").innerHTML = 'bar'; return false; }
+function clickHandler(e) { toggle(); }
+
+var iconStatus = 0;
+
+function toggle() {
+  // myDebug('mydebugging');
+  if (iconStatus == 0){
+    // chrome.browserAction.setBadgeText({text: "up"}); // so it's set for the tab user is in
+    chrome.browserAction.setIcon({path: "female_red_icon.png"}); // so it's set for the tab user is in
+    iconStatus++;
+  }
+  else if (iconStatus == 1){
+    // chrome.browserAction.setBadgeText({text: "down"}); // so it's set for the tab user is in
+    chrome.browserAction.setIcon({path: "female_green_icon.png"});
+    iconStatus = 0;
+  }
+}
